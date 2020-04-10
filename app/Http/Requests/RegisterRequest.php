3ef8e7f2348cdd19e2539cde98abd6c9d81d0294
irisviana <vianasantana21@gmail.com>
2020-04-09 23:16:51 -0300

    adicionando arquivo esquecido de validacao de regitro de usuario

---
+++ app/Http/Requests/RegisterRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => ['required', 'string','min:3', 'max:255'],
+            'password' => ['required', 'string', 'min:6','confirmed'],
+	    
+        ];
+    }
+    public function messages(){
+	return[
+	    'name.required'=>'Nome deve ser obrigatório',
+	    'name.string'=>'Nome não pode conter números',
+	    'name.min'=>'Nome deve conter no mínimo três caracteres',
+	    'password.required'=>'Senha deve ser obrigatória',
+	    'password.min'=>'Senha deve conter no mínimo seis caracteres',
+	    'password.confirmed'=>'Senhas não conferem',
+        ];
+    }
+}
