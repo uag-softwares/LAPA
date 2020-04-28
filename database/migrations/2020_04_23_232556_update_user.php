7bfa3e0cacbf736a13bdd5ee8bb0eb4dc934bdf5
irisviana <vianasantana21@gmail.com>
2020-04-28 00:37:53 -0300

    adicionando novos atributos ao usuário

---
+++ database/migrations/2020_04_23_232556_update_user.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateUser extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('telephone')->nullable();
+	    $table->string('surname');
+	    $table->string('user_description')->nullable();
+	    $table->string('avatar')->nullable();
+	    $table->enum('user_type', ['admin', 'visitant']);
+	    $table->timestamp('cpf_verified_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+}
