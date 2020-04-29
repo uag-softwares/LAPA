162a7336bd1b28b1f1e9172a1112cf7c5273acf5
raquel <raquellvieiraa@gmail.com>
2020-04-29 13:12:21 -0300

    Corrigido erro do codacy arquivo de mascaras.

--- public/js/masks.js
+++ public/js/masks.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
-		$('.cpf').inputmask('999.999.999-99');
-    $('.cnpj').inputmask('99.999.999/9999-99', {reverse: true});
-    $('.telefone').inputmask('(99) 99999-9999');
-    $('.cep').inputmask('99999-999');
+		$(".cpf").inputmask("999.999.999-99");
+    $(".cnpj").inputmask("99.999.999/9999-99", {reverse: true});
+    $(".telefone").inputmask("(99) 99999-9999");
+    $(".cep").inputmask("99999-999");
     $(".dinheiro").maskMoney({
         prefix: "",
         decimal: ",",
