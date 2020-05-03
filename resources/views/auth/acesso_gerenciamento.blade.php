852f4bdfec7008d74e235e032877cf8bfd5184fe
irisviana <vianasantana21@gmail.com>
2020-05-03 16:43:04 -0300

    adicionando menssagem de sucesso de usuário e solicitar acesso

--- resources/views/auth/acesso_gerenciamento.blade.php
+++ resources/views/auth/acesso_gerenciamento.blade.php
@@ -2,13 +2,16 @@
 
 @section('titulo', 'Gerenciar Solicitações de Registros')
 @section('content')
-       @if (session('warning'))
-                        <div class="alert alert-warning">
-                            {{ session('warning') }}
-                        </div>
-                    @endif
         <div class="container">
             <h2>Gerenciar Solicitações de Registros</h2>
+            @if(Session::has('success'))
+                <div class="alert alert-success alert-dismissible">
+                    {{ Session::get('success') }}
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+            @endif
             <table class="table">
                 <thead>
                     <tr>
