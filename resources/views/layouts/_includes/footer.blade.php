8090eb8fc3c8b577b65870a792d883161bd4cf55
raquel <raquellvieiraa@gmail.com>
2020-05-18 23:19:45 -0300

    Mudança no footer.

--- resources/views/layouts/_includes/footer.blade.php
+++ resources/views/layouts/_includes/footer.blade.php
@@ -1,10 +1,39 @@
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
 <!-- Footer -->
-<footer class="page-footer font-small cyan darken-3">
-<div class="item-green">
-  <div class="container">
-    <div class="row">
-    
-      <div class="col-md-12 py-5">
+	<section id="footer">
+		<div class="container">
+			<div class="row text-center text-xs-center text-sm-left text-md-left">
+				<div class="col-xs-12 col-sm-4 col-md-4">
+                    <h5>LAPA</h5>
+                    <p>Laboratório de Anatomia<br/>e Patologia Animal</p>
+                    <p>Avenida Bom Pastor, s/n.º<br />Bairro Boa Vista - CEP:<br />
+                        55292-270 - Garanhuns - PE</p>
+				</div>
+				<div class="col-xs-12 col-sm-4 col-md-4">
+					<h5>Navegação</h5>
+					<ul class="list-unstyled quick-links">
+						<li><a href="#"><i class="fa fa-angle-double-right"></i>Atlas Iterativo</a></li>
+						<li><a href="#"><i class="fa fa-angle-double-right"></i>Visitas</a></li>
+						<li><a href="#"><i class="fa fa-angle-double-right"></i>Eventos</a></li>
+						<li><a href="#"><i class="fa fa-angle-double-right"></i>Materiais</a></li>
+						<li><a href="#"><i class="fa fa-angle-double-right"></i>Quem somos</a></li>
+					</ul>
+				</div>
+				<div class="col-xs-12 col-sm-4 col-md-4">
+					<h5>Contatos</h5>
+					<ul class="list-unstyled quick-links">
+						<li><a href="#"><i class="fa fa-angle-double-right"></i>Home</a></li>
+						<li><a href="#"><i class="fa fa-angle-double-right"></i>About</a></li>
+						<li><a href="#"><i class="fa fa-angle-double-right"></i>FAQ</a></li>
+					</ul>
+				</div>
+			</div>
+			   
+    <div class="col-md-12 py-5">
         <div class="mb-5 flex-center">
           <!-- Facebook -->
           <a class="facebook-icone">
@@ -23,15 +52,7 @@
             <i class="fab fa-pinterest fa-lg white-text fa-2x"> </i>
           </a>
         </div>
-      </div>
+        <p>© 2020 Todos os direitos reservados.</p>
     </div>
-  </div>
-
-  <div class="title-rodape" style="text-align:center">
-    <h1>Laboratório de Anatomia e Patologia Animal</h3>
-  </div>
-</div>
-
-  <!-- Copyright -->
-  <div class="footer-copyright text-center py-3">© 2020 Todos os direitos reservados. </div>
-</footer>
+ </section>
+<!-- ./Footer -->
