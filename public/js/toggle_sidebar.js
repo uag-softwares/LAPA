6e01c8961eb0118d10b098acbb20e0810f829963
vinesnts <v.santos0406@gmail.com>
2020-05-18 23:21:08 -0300

    Corrigido erro do codacy no toggle_sidebar.js

--- public/js/toggle_sidebar.js
+++ public/js/toggle_sidebar.js
@@ -1,17 +1,17 @@
 // Image to Lightbox Overlay 
-$('.img').on('click', function() {
-$('#overlay')
+$(".img").on("click", function() {
+$("#overlay")
     .css({backgroundImage: `url(${this.src})`})
-    .addClass('open')
-    .one('click', function() { $(this).removeClass('open'); });
+    .addClass("open")
+    .one("click", function() { $(this).removeClass("open"); });
 });
 
 // Toggle pages on mobile
-$('#toggleLeftSidebar').on('click', function() {
-    $('#leftSidebar').toggleClass('d-none', 'position-absolute', 'show');
-    $('#leftSidebar').toggleClass('position-absolute');
-    $('#leftSidebar').toggleClass('show');
-    $('#toggleLeftSidebar').toggleClass('push');
-    $('#toggleLeftSidebar a span').toggleClass('fa-chevron-right');
-    $('#toggleLeftSidebar a span').toggleClass('fa-chevron-left');
+$("#toggleLeftSidebar").on("click", function() {
+    $("#leftSidebar").toggleClass("d-none", "position-absolute", "show");
+    $("#leftSidebar").toggleClass("position-absolute");
+    $("#leftSidebar").toggleClass("show");
+    $("#toggleLeftSidebar").toggleClass("push");
+    $("#toggleLeftSidebar a span").toggleClass("fa-chevron-right");
+    $("#toggleLeftSidebar a span").toggleClass("fa-chevron-left");
 });
