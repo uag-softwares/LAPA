f460f0e7d442caa3ffddf360add3feeaaea4948a
raquel <raquellvieiraa@gmail.com>
2020-05-18 23:53:10 -0300

    corrigindo erros do cadacy

--- public/js/summernote_config.js
+++ public/js/summernote_config.js
@@ -1,21 +1,21 @@
 $(document).ready(function() {
-    $('#summernote').summernote({
-        lang: 'pt-BR',
-        placeholder: 'Digite aqui o texto',
+    $("#summernote").summernote({
+        lang: "pt-BR",
+        placeholder: "Digite aqui o texto",
         tabsize: 2,
         height: 300,
         toolbar: [
-            ['view', ['undo', 'redo',]],
-            ['style', ['style']],
-            ['font', ['bold', 'underline', 'italic', 'clear', 'fontsize']],
-            ['color', ['color']],
-            ['para', ['ul', 'ol', 'paragraph']],
-            ['table', ['table']],
-            ['insert', ['link', 'picture', 'video']],
-            ['view', ['fullscreen', 'help']]
+            ["view", ["undo", "redo",]],
+            ["style", ["style"]],
+            ["font", ["bold", "underline", "italic", "clear", "fontsize"]],
+            ["color", ["color"]],
+            ["para", ["ul", "ol", "paragraph"]],
+            ["table", ["table"]],
+            ["insert", ["link", "picture", "video"]],
+            ["view", ["fullscreen", "help"]]
         ],
         styleTags: [
-            'p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6'
+            "p", "h1", "h2", "h3", "h4", "h5", "h6"
         ],
     });
 });
