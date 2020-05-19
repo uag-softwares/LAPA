49276e547c9e1634be354fbfdf3ba17ad0b8cc61
raquel <raquellvieiraa@gmail.com>
2020-05-18 22:11:21 -0300

    Melhora no footer.

--- resources/views/layouts/_includes/footer.blade.php
+++ resources/views/layouts/_includes/footer.blade.php
@@ -1,22 +1,21 @@
 <!-- Footer -->
 <footer class="page-footer font-small cyan darken-3">
-
+<div class="item-green">
   <div class="container">
     <div class="row">
-
+    
       <div class="col-md-12 py-5">
         <div class="mb-5 flex-center">
-
           <!-- Facebook -->
-          <a class="fb-ic">
+          <a class="facebook-icone">
             <i class="fab fa-facebook-f fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
           </a>
           <!-- Twitter -->
-          <a class="tw-ic">
+          <a class="twitter-icone">
             <i class="fab fa-twitter fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
           </a>
           <!--Instagram-->
-          <a class="ins-ic">
+          <a class="instagran-icone">
             <i class="fab fa-instagram fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
           </a>
           <!--Pinterest-->
@@ -27,8 +26,12 @@
       </div>
     </div>
   </div>
-  <!-- Copyright -->
-  <div class="footer-copyright text-center py-3">
-        <p>© 2020 Todos os direitos reservados.</p>
+
+  <div class="title-rodape" style="text-align:center">
+    <h1>Laboratório de Anatomia e Patologia Animal</h3>
   </div>
+</div>
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center py-3">© 2020 Todos os direitos reservados. </div>
 </footer>
