f4d229197d27ff156647a9479095fb3dbb4c074e
vinesnts <v.santos0406@gmail.com>
2020-05-19 21:36:25 -0300

    Corrigido layout do header nos emails

--- resources/views/vendor/mail/html/message.blade.php
+++ resources/views/vendor/mail/html/message.blade.php
@@ -2,9 +2,9 @@
 {{-- Header --}}
 @slot('header')
 @component('mail::header', ['url' => config('app.url')])
-    <div class="d-flex justify-content-center">
-        <img src="{{ asset('img/lapa-logo-light.png') }}" class="logo align-self-end " alt="Logo do laboratório de anatomia e patologia animal" style="max-width: 400px; width: 85%;">
-        <img src="{{ asset('img/logo-uag-light.png') }}" class="logo align-self-center " alt="Logo da Universidade Federal do Agreste de Pernambuco" style="max-width: 55px; width: 15%;">
+    <div style="display: flex; justify-content: center; padding: 2em;">
+        <img src="{{ asset('img/lapa-logo-light.png') }}" class="logo" alt="Logo do laboratório de anatomia e patologia animal" style="max-width: 400px; width: 85%; align-self: flex-end;">
+        <img src="{{ asset('img/logo-uag-light.png') }}" class="logo" alt="Logo da Universidade Federal do Agreste de Pernambuco" style="max-width: 55px; width: 15%; align-self: center;">
     </div>
 @endcomponent
 @endslot
