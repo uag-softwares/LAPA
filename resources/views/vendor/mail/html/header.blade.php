900948581d7e2a288aecffadfe190c9e8c1c1e6e
vinesnts <v.santos0406@gmail.com>
2020-05-19 22:26:38 -0300

    Corrigido layout de email no header

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,11 +1,13 @@
 <tr>
     <td class="header">
-        <a href="{{ $url }}">
-            @if (trim($slot) === 'Laravel')
-                <img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
-            @else
-                {{ $slot }}
-            @endif
-        </a>
+        <div style="display: flex; padding: 2em;">
+            <a href="{{ $url }}" style="margin: 0 auto;">
+                @if (trim($slot) === 'Laravel')
+                    <img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
+                @else
+                    {{ $slot }}
+                @endif
+            </a>
+        </div>
     </td>
 </tr>
