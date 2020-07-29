8585de5953802543280567a8c721fc14311843b8
raquel <raquellvieiraa@gmail.com>
2020-07-29 20:34:17 -0300

    Corrigidos erros apontados pelo codacy.

--- public/js/summernote_atlas_config.js
+++ public/js/summernote_atlas_config.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
-    $('#summernote_atlas').summernote({
-        lang: 'pt-BR',
-        placeholder: 'Digite aqui o texto',
+    $("#summernote_atlas").summernote({
+        lang: "pt-BR",
+        placeholder: "Digite aqui o texto",
         tabsize: 2,
         height: 200,
         toolbar: [
-            ['view', ['undo', 'redo',]],
-            ['font', ['bold', 'underline', 'italic']],
+            ["view", ["undo", "redo",]],
+            ["font", ["bold", "underline", "italic"]],
            
         ],
        
