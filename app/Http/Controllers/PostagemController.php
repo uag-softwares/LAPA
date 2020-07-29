d9a2ca1205ac62936667f9d2f44f1f1f8abf63a5
vinesnts <v.santos0406@gmail.com>
2020-07-29 16:41:49 -0300

    Corrigido erro do codacy

--- app/Http/Controllers/PostagemController.php
+++ app/Http/Controllers/PostagemController.php
@@ -23,8 +23,6 @@ class PostagemController extends Controller
                                   'siteHome','siteIndexEvento','siteIndexEdital']]);
         $this->postagem = $postagem;
         $this->user = $user;
-
-        setlocale(LC_ALL, 'pt_BR', 'pt_BR.utf-8', 'pt_BR.utf-8', 'portuguese');
     }
 
     public function index() 
