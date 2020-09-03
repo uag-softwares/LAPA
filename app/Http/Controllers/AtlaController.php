73ae3ed42e564e931ebc491dc514927d7019b2bd
raquel <raquellvieiraa@gmail.com>
2020-09-03 19:10:48 -0300

    Alterações no AtlasController funcionalidade adc anexo via link do drive.

--- app/Http/Controllers/AtlaController.php
+++ app/Http/Controllers/AtlaController.php
@@ -44,6 +44,7 @@ class AtlaController extends Controller
             $filtros['publicado'] = request('publicado');
             $filtros['nomes'] = [request('publicado') ? 'publicados' : 'rascunhos'];
         }
+
         if(request()->has('categoria') && request('categoria') != '') {
             $registros = $registros->where('categoria_id', request('categoria'));
             $filtros['categoria'] = request('categoria');
@@ -51,7 +52,6 @@ class AtlaController extends Controller
         }
 
         $categorias = $this->categoria->all();
-
         $registros = $registros->latest()->get();
         return view('auth.atlas.index', compact('registros', 'categorias', 'filtros'));
     }
@@ -66,27 +66,39 @@ class AtlaController extends Controller
     public function salvar(CriarAtlaRequest $request) 
     {
         $request->validated();
-        $dados = $request->all();
-
-        if(isset($dados['publicar'])) {
-            $dados['publicado'] = true;
-        } else if(isset($dados['rascunho'])) {
-            $dados['publicado'] = false;    
-        }
-
-        $atla=$this->atla->create($dados);
+        $this->user = Auth::user();
+        $anexo = null;
+        $publicado = false;
+      
+        if(isset($request['publicar'])) {
+            $publicado = true;
+        }   
         
+        $atla=$this->atla->create([
+            'titulo' => $request ['titulo'],
+            'descricao' => $request ['descricao'],
+            'anexo' => $anexo,
+            'publicado'=>$publicado,
+            'categoria_id' =>$this->categoria->id,
+        ]);
         $atla['slug'] = str_slug($atla->titulo).'-'.$atla->id;
 
-        if($request->hasFile('anexo')) {
-            $anexo = $request->file('anexo');
+         /* Se o link for web nao entra no if
+        * se for drive ele entra no if para converter o link para ser embarcado na página
+        * se for upload ele entra no else if para arrumar o nome do arquivo
+        */
+        $atla['anexo'] = $request['anexo_web'];
+        if($request['tipo_anexo'] == 'link_drive') {
+            $atla['anexo'] = $this->atla::convertToEmbedableImageLink($request['anexo_drive']);
+        }else if (($request['tipo_anexo'] == 'upload') && $request->hasFile('anexo_upload')) {
+            $anexo = $request->file('anexo_upload');
             $dir = 'img/atlas/';
-            $extensao = $anexo->guessClientExtension(); //Define a extensao do arquivo
-            $nomeAnexo = 'anexo_'.$atla['slug'].'.'.$extensao;
+            $ex = $anexo->guessClientExtension(); //Define a extensao do arquivo
+            $nomeAnexo = 'anexo_'.$atla->anexo.'-'.$atla->id.'.'.$ex;
             $anexo->move($dir, $nomeAnexo);
             $atla['anexo'] = $dir.'/'.$nomeAnexo;
         }
-
+       
         $atla->update($atla->attributesToArray());
         return redirect()->route('auth.atlas')->with('success', 'Página do atlas adicionada com sucesso!');
         
@@ -102,27 +114,29 @@ class AtlaController extends Controller
     public function atualizar(AtualizarAtlaRequest $request, $identifier)
     {
         $request->validated();
-
         $dados = $request->all();
-
-        if(isset($dados['publicar'])) {
-            $dados['publicado'] = true;
-        } else if(isset($dados['rascunho'])) {
-            $dados['publicado'] = false;    
-        }
-
-        $dados['slug'] = str_slug($dados['titulo']).'-'.$identifier;
-
-        if($request->hasFile('anexo')) {
-            $anexo = $request->file('anexo');
-            $dir = 'img/atlas';
-            $ex = $anexo->guessClientExtension(); //Define a extensao do arquivo
-            $nomeAnexo = 'anexo_'.$dados['slug'].'.'.$ex;
-            $anexo->move($dir, $nomeAnexo);
-            $dados['anexo'] = $dir.'/'.$nomeAnexo;
-        }
-
+        $dados['publicado'] = false;
         $this->atla->find($identifier)->update($dados);
+       
+
+        $dados['anexo'] = $request['anexo_web'];
+        $dados['tipo_anexo'] = $request['tipo_anexo'];
+        if($request['tipo_anexo'] == 'link_drive') {
+            $dados['anexo'] = $this->atla::convertToEmbedableImageLink($request['anexo_drive']);
+        } else if(($request['tipo_anexo'] == 'upload') && $request->hasFile('anexo_upload')) {
+            $anexo = $request->file('anexo_upload');
+            $dir = 'img/atlas/';
+            $ex = $anexo->guessClientExtension(); //Define a extensao do arquivo
+            $nomeAnexo = 'anexo_'.$atla->anexo.'-'.$atla->id.'.'.$ex;
+            $anexo->move($dir, $nomeAnexo);
+            $dados['anexo']= $dir.'/'.$nomeAnexo;
+        }
+
+        if(isset($request['publicar'])) {
+            $dados['publicado'] = true;
+        } 
+        $dados['slug'] = str_slug($dados['titulo']).'-'.$identifier;
+        $atla->update($dados);
         return redirect()->route('auth.atlas')->with('success', 'Página do atlas atualizada com sucesso!');
     }
 
