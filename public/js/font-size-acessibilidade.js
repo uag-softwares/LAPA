d009ab5de1ea7c1a32ea400c2a71abae06d19f19
raquel <raquellvieiraa@gmail.com>
2020-09-03 18:37:50 -0300

    Adicionado acessibilidade quanto tamanho da fonte.

---
+++ public/js/font-size-acessibilidade.js
@@ -0,0 +1,34 @@
+window.onload = function() {
+    var elementBody = document.querySelector(':root');
+    var elementBtnIncreaseFont = document.getElementById('increase-font');
+    var elementBtnDecreaseFont = document.getElementById('decrease-font');
+    var elementBtnReseteFont = document.getElementById('resete-font');
+
+    // Padrão de tamanho, equivale a 100% do valor definido no Body
+    var fontSize = 100;
+    // Valor de incremento ou decremento, equivale a 10% do valor do Body
+    var increaseDecrease = 10;
+
+    // Evento de click para aumentar a fonte
+    elementBtnIncreaseFont.addEventListener('click', function(event) {
+        if(fontSize <= 140){
+            fontSize = fontSize + increaseDecrease;
+            elementBody.style.fontSize = fontSize + '%';
+        }
+       
+    });
+
+     // Evento de click para resetar a fonte
+     elementBtnReseteFont.addEventListener('click', function(event) {
+        elementBody.style.fontSize = '100%';     
+    });
+
+    // Evento de click para diminuir a fonte
+    elementBtnDecreaseFont.addEventListener('click', function(event) {
+        if(fontSize >= 80){
+            fontSize = fontSize - increaseDecrease;
+            elementBody.style.fontSize = fontSize + '%';
+        }
+
+    });
+}
