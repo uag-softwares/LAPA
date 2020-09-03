64e010482d80c4825f88bdcb8d2d6132043d76da
raquel <raquellvieiraa@gmail.com>
2020-09-03 20:50:49 -0300

    Correção nos animes de input link do drive em Atlas.

--- resources/views/auth/atlas/_form.blade.php
+++ resources/views/auth/atlas/_form.blade.php
@@ -12,7 +12,7 @@
     @enderror
 </div>
 <div class="form-group">
-    <label for="descricao">Descrição da página*</label>
+    <label for="descricao">Descrição*</label>
     <textarea rows="14" id="summernote" class="form-control form-control-lg @error('descricao') is-invalid @enderror" type="text" name="descricao"  required autocomplete="descricao">{{ isset($registro->descricao) ? $registro->descricao : old('descricao') }}</textarea>
     @error('descricao')
         <span class="invalid-feedback" role="alert">
@@ -55,8 +55,8 @@
     @enderror
 </div>
 
-<div class="form-group">
-    <label id="upload" class="file-input w-100" style="{{ isset($registro) ? ($registro->tipo_anexo == 'upload' ? 'display: block' : 'display: none') : 'display: none' }}" for="anexo">
+<div class="form-group form-group-anime {{ isset($registro) ? 'show' : '' }}">
+    <label id="upload" class="file-input w-100 input-anime {{ isset($registro) ? ($registro->tipo_anexo == 'link_drive' ? 'show' : '') : '' }}">
         <div class="d-flex flex-column text-center border rounded bg-white">
             <div class="file-header">
                 <img height="200px" id="img-foto" src="{{ asset($registro->anexo ?? asset('img/file-image.svg')) }}" alt="" style="max-height: 200px">
@@ -67,12 +67,12 @@
         </div>
         <input id="anexo" class="d-none form-control form-control-lg @error('anexo') is-invalid @enderror" type="file" name="anexo_upload" placeholder="Escolha um arquivo jpeg, jpg, png ou gif" onchange="document.getElementById('img-foto').src = window.URL.createObjectURL(this.files[0])">
     </label>
-    <div id="link_drive" class="drive-input" style="{{ isset($registro) ? ($registro->tipo_anexo == 'link_drive' ? 'display: block' : 'display: none') : 'display: none' }}">
+    <div id="link_drive"  class="drive-input input-anime {{ isset($registro) ? ($registro->tipo_anexo == 'link_drive' ? 'show' : '') : '' }}">
         <label>Link da imagem do Google Drive*</label>
         <input type="text" class="form-control form-control-lg @error('anexo') is-invalid @enderror" name="anexo_drive" placeholder="A imagem deve ser no formato jpeg, jpg, png ou gif." value="{{ isset($registro->anexo) ? $registro->anexo : old('anexo') }}">
         <p class="info">*O link é obtido na opção "Gerar link compartilhável" pelo Google Drive e deve ter a permissão "Visível a qualquer pessoa com link".</p>
     </div>
-    <div id="link_web" class="web-link-input" style="{{ isset($registro) ? ($registro->tipo_anexo == 'link_web' ? 'display: block' : 'display: none') : 'display: none' }}">
+    <div id="link_web" class="web-link-input input-anime {{ isset($registro) ? ($registro->tipo_anexo == 'link_drive' ? 'show' : '') : '' }}">
         <label>Link da imagem da web</label>
         <input type="text" class="form-control form-control-lg @error('anexo') is-invalid @enderror" name="anexo_web" placeholder="A imagem deve ser no formato jpeg, jpg, png ou gif." value="{{ isset($registro->anexo) ? $registro->anexo : old('anexo') }}">
     </div>	   
@@ -85,3 +85,9 @@
 <div class="form-group">
     <p>Área de conhecimento não cadastrada? <a class="" href="{{ route('auth.categoria.adicionar') }}">{{ __('Cadastrar Área de conhecimento') }}</a>.</p>
 </div>
+
+@section('scripts')
+  <!-- Script de mostrar/esconder campos de anexos -->
+  <script src="{{ asset('js/toggle_anexo_input.js') }}"></script>
+
+@endsection
