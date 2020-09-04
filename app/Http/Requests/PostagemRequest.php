59a4ba09e441eb8321e50a72d6f9fc8636efccdb
vinesnts <v.santos0406@gmail.com>
2020-09-04 17:27:10 -0300

    Corrigidas mensagens de erros em postagens

--- app/Http/Requests/PostagemRequest.php
+++ app/Http/Requests/PostagemRequest.php
@@ -26,12 +26,13 @@ class PostagemRequest extends FormRequest
         return [
             'titulo' => 'required|min:5|max:255',
             'descricao' => 'required|min:10',
-            'data' => 'nullable|date|after:today',
-            'hora' => 'nullable|date_format:H:i|after_or_equal:00:00',
-            'tipo_anexo' => 'required',
-            //'anexo' => 'mimes:jpeg,jpg,png,gif|max:2048|nullable',
             'tipo_postagem'=>'required',
-            
+            'data' => 'required_if:tipo_postagem,evento|nullable|date|after:today',
+            'hora' => 'required_if:tipo_postagem,evento|nullable|date_format:H:i|after_or_equal:00:00',
+            'tipo_anexo' => 'required',
+            'anexo_upload' => 'required_if:tipo_anexo,upload|mimes:jpeg,jpg,png,gif|max:2048|nullable',
+            'anexo_drive' => 'required_if:tipo_anexo,link_drive|nullable',
+            'anexo_web' => 'required_if:tipo_anexo,link_web|nullable',
         ];
     }
    public function messages(){
@@ -39,6 +40,10 @@ class PostagemRequest extends FormRequest
             'tipo_postagem.required'=>'Selecionar o tipo da postagem é obrigatório', 
 	        'hora.date_format'=>" A hora selecionada não está no formato H:i",
             'data.after'=>'A data selecionada tem que ser posterior a hoje',
+            'anexo_upload.required_if' => 'É necessário enviar um arquivo quando a opção for selecionada, selecione a opção desejada novamente.',
+            'anexo_web.required_if' => 'É necessário enviar um link de imagem do Google Drive quando a opção for selecionada, selecione a opção desejada novamente.',
+            'anexo_drive.required_if' => 'É necessário enviar um link de imagem da web quando a opção for selecionada, selecione a opção desejada novamente.',
+
         ];
     }
     
--- public/js/toggle_anexo_input.js
+++ public/js/toggle_anexo_input.js
@@ -4,18 +4,20 @@ for( var i = 0, length = radioGroupAnexo.length; i < length; i++) {
     radioGroupAnexo[parseInt(i, 10)].addEventListener("change", function() {
         if(this.checked) {
             document.querySelector(".form-group-anime").classList.add("show");
+            var upload = document.getElementById("upload");
+            var linkWeb = document.getElementById("link_web");
+            var linkDrive = document.getElementById("link_drive");
+
+            upload.classList.remove("show");
+            linkWeb.classList.remove("show");
+            linkDrive.classList.remove("show");
+            
             if(this.value === "link_drive") {
-                document.getElementById("link_drive").classList.add("show");
-                document.getElementById("link_web").classList.remove("show");
-                document.getElementById("upload").classList.remove("show");
+                linkDrive.classList.add("show");
             } else if(this.value === "link_web") {
-                document.getElementById("link_drive").classList.remove("show");
-                document.getElementById("link_web").classList.add("show");
-                document.getElementById("upload").classList.remove("show");
+                linkWeb.classList.add("show");
             } else if(this.value === "upload") {
-                document.getElementById("link_drive").classList.remove("show");
-                document.getElementById("link_web").classList.remove("show");
-                document.getElementById("upload").classList.add("show");
+                upload.classList.add("show");
             }
         }
     });
