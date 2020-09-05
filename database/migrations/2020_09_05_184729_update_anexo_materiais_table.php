13657c26052ec85dd9c0bf47110d7168363baad1
irisviana <vianasantana21@gmail.com>
2020-09-05 15:59:05 -0300

    modificado atributo anexo e avatar pra longtext

---
+++ database/migrations/2020_09_05_184729_update_anexo_materiais_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateAnexoMateriaisTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('materials', function (Blueprint $table) {
+            $table->longtext('anexo')->nullable(false)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('materials', function (Blueprint $table) {
+            //
+        });
+    }
+}
