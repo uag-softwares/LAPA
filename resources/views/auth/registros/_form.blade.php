da263b8b7107f7f8d95f070f5201e2b20c9492f7
vinesnts <v.santos0406@gmail.com>
2020-09-05 17:59:22 -0300

    Corrigido validacao em usuario

--- resources/views/auth/registros/_form.blade.php
+++ resources/views/auth/registros/_form.blade.php
@@ -1,25 +1,25 @@
 @php($user = Auth::user())
-   @if(@isset($user->avatar))
+   @if(isset($user->avatar))
           <div class="input-field">
              <img src="{{asset($user->avatar) }}" alt="" height="200" width="200">
           </div>    
     @endisset
     <div class="form-group d-flex flex-column align-items-left">
-      <div id="radio-group-anexo">
-         <label for="avatar">{{ __('Foto (opcional)') }}</label>
-              <label class="@error('tipo_avatar') is-invalid @enderror">Escolher origem da imagem anexa*</label><br>
-          <input disabled type="radio" name="tipo_avatar" value="upload" id="upload-radio" {{ isset($user) ? ($user->tipo_avatar== 'upload' ? 'checked' : '') : ''}}>
-          <label for="upload-radio">Enviar arquivo do dispositivo</label><br>
-          <input type="radio" name="tipo_avatar" value="link_drive" id="drive-radio" {{ isset($user) ? ($user->tipo_avatar== 'link_drive' ? 'checked' : '') : '' }}>
-          <label for="drive-radio">Link compartilhado do Google Drive</label><br>
-          <input type="radio" name="tipo_avatar" value="link_web" id="web-radio" {{ isset($user) ? ($user->tipo_avatar == 'link_web' ? 'checked' : '') : '' }}>
-          <label for="web-radio">Link da imagem da web</label>
-          @error('tipo_avatar')
-              <span class="invalid-feedback" role="alert">
-                  <strong>{{ $message }}</strong>
-              </span>
-          @enderror
-      </div> 
+        <div id="radio-group-anexo">
+            <label for="avatar">{{ __('Foto (opcional)') }}</label>
+            <label class="@error('tipo_avatar') is-invalid @enderror @error('anexo_web') is-invalid @enderror @error('anexo_drive') is-invalid @enderror @error('anexo_upload') is-invalid @enderror">Escolher origem da imagem anexa*</label><br>
+            <input disabled type="radio" name="tipo_avatar" value="upload" id="upload-radio" {{ isset($user) ? ($user->tipo_avatar== 'upload' ? 'checked' : '') : ''}}>
+            <label for="upload-radio">Enviar arquivo do dispositivo</label><br>
+            <input type="radio" name="tipo_avatar" value="link_drive" id="drive-radio" {{ isset($user) ? ($user->tipo_avatar== 'link_drive' ? 'checked' : '') : '' }}>
+            <label for="drive-radio">Link compartilhado do Google Drive</label><br>
+            <input type="radio" name="tipo_avatar" value="link_web" id="web-radio" {{ isset($user) ? ($user->tipo_avatar == 'link_web' ? 'checked' : '') : '' }}>
+            <label for="web-radio">Link da imagem da web</label>
+            @if($errors->first('tipo_avatar') || $errors->first('anexo_upload') || $errors->first('anexo_drive') || $errors->first('anexo_web'))
+                <span class="invalid-feedback" role="alert">
+                    <strong>{{ $errors->first('tipo_avatar') | $errors->first('anexo_upload') | $errors->first('anexo_drive') | $errors->first('anexo_web')}}</strong>
+                </span>
+            @endif
+        </div> 
     </div>
     <div class="form-group form-group-anime {{ isset($user) ? 'show' : '' }}">
     <label id="upload" class="file-input w-100 input-anime {{ isset($user) ? ($user->tipo_avatar == 'upload' ? 'show' : '') : '' }}" for="avatar">
@@ -31,16 +31,16 @@
                 <p>Escolher uma imagem jpeg, jpg, png ou gif.</p>
             </div>
         </div>
-        <input id="avatar" class="d-none form-control form-control-lg @error('avatar') is-invalid @enderror" type="file" name="anexo_upload" placeholder="Escolha um arquivo jpeg, jpg, png ou gif" onchange="document.getElementById('img-foto').src = window.URL.createObjectURL(this.files[0])">
+        <input id="avatar" class="d-none form-control form-control-lg" type="file" name="anexo_upload" placeholder="Escolha um arquivo jpeg, jpg, png ou gif" onchange="document.getElementById('img-foto').src = window.URL.createObjectURL(this.files[0])">
     </label>
     <div id="link_drive"  class="drive-input input-anime {{ isset($user) ? ($user->tipo_avatar == 'link_drive' ? 'show' : '') : '' }}">
         <label>Link da imagem do Google Drive*</label>
-        <input type="text" class="form-control form-control-lg @error('avatar') is-invalid @enderror" name="anexo_drive" placeholder="A imagem deve ser no formato jpeg, jpg, png ou gif." value="{{ isset($user->avatar) ? $user->avatar : old('avatar') }}">
+        <input type="text" class="form-control form-control-lg" name="anexo_drive" placeholder="A imagem deve ser no formato jpeg, jpg, png ou gif." value="{{ isset($user->avatar) ? $user->avatar : old('avatar') }}">
         <p class="info">*O link é obtido na opção "Gerar link compartilhável" pelo Google Drive e deve ter a permissão "Visível a qualquer pessoa com link".</p>
     </div>
     <div id="link_web" class="web-link input-anime {{ isset($user) ? ($user->tipo_avatar == 'link_web' ? 'show' : '') : '' }}">
         <label>Link da imagem da web</label>
-        <input type="text" class="form-control form-control-lg @error('avatar') is-invalid @enderror" name="anexo_web" placeholder="A imagem deve ser no formato jpeg, jpg, png ou gif." value="{{ isset($user->avatar) ? $user->avatar : old('avatar') }}">
+        <input type="text" class="form-control form-control-lg" name="anexo_web" placeholder="A imagem deve ser no formato jpeg, jpg, png ou gif." value="{{ isset($user->avatar) ? $user->avatar : old('avatar') }}">
     </div>     
     @error('avatar')
         <span class="invalid-feedback" role="alert">
@@ -111,7 +111,7 @@
     
     <div class="form-group">
         <label for="psw-repeat">Confirme a senha*</label>									
-        <input id="password-confirm" type="password" class="form-control form-control-lg" name="password_confirmation" value="{{isset($user->password_confirmation) ? $user->password_confirmation: ''}}" required autocomplete="new-password">
+        <input id="password-confirm" type="password" class="form-control form-control-lg" name="password_confirmation" value="{{isset($user->password_confirmation) ? $user->password_confirmation : '' }}" required autocomplete="new-password">
          @error('password_confirmation')
             <span class="invalid-feedback" role="alert">
                 <strong>{{ $message }}</strong>
